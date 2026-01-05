d0b9496cc4526797146ae637d2875886fab0fcb6
Prathamesh More <prathamesh4244@gmail.com>
2026-01-05 21:08:16 +0530

    Initial upload: Day 1

---
+++ lecture one/trail.js
@@ -0,0 +1,36 @@
+//Practice Questions Easy Lvl
+
+//1st
+console.log("Hello, I'm your Coding fun fact guide")
+
+//2nd
+let botName="gemini";
+let  botLocation="india'";
+let favoriteLanguage="jabha-e-script";
+
+//3rd
+all = `'My name is' "${botName}" "${botLocation}"  `
+console.log(all);
+
+//4th
+tall = `'My favorite language is '"${favoriteLanguage}"`
+console.log(tall)
+
+//5th
+let codingFact={
+    codingPython: "python is interpreted Language"
+}
+
+//6th
+console.log(codingFact);
+
+//7th
+codingFact= `Fav Language  ${favoriteLanguage} javascript is pronounced as jabha-e-script`
+
+//8th
+console.log(codingFact);
+
+//9th is same 
+
+//10th
+console.log=(`It was great day with you ${botName}`);
