96281f78116a77a63cf22b3765e1ce561f098ed5
Prathamesh More <prathamesh4244@gmail.com>
2026-02-03 21:24:16 +0530

    added assignment 2

--- uni assignments/assignment 2/script.js
+++ uni assignments/assignment 2/script.js
@@ -1,31 +1,32 @@
-const storyContainer = document.querySelector('.story-container');
 
-const scaryStoryBtn = document.querySelector('#scary-btn');
-const funnyStoryBtn = document.querySelector('#funny-btn');
-const adventureStoryBtn = document.querySelector('#adventure-btn');
-
-const resultParagraph = document.querySelector('#result');
+const storyContainer = document.querySelector(".story-container");
+const scaryStoryBtn = document.querySelector("#scary-btn");
+const funnyStoryBtn = document.querySelector("#funny-btn");
+const adventureStoryBtn = document.querySelector("#adventure-btn");
+const resultParagraph = document.querySelector("#result");
 
 const storyObj = {
     scary: {
-        story: "In the dark woods, a group of friends stumbled upon an old abandoned cabin. They entered the cabin and awakened something malevolent that had been dormant for centuries.",
+        story: "In the dark woods, a group of friends stumbled upon an old, abandoned cabin. They enter the cabin and awaken something malevolent that had been dormant for centuries.",
         borderColor: "#ee4b2b"
     },
     funny: {
-        story: "A man slipped on a banana peel, stood up, slipped again, and decided bananas were his mortal enemy.",
-        borderColor: "yellow"
+        story: "During a camping trip, Mark decided to show off his culinary skills by cooking dinner over an open fire. However, his attempt caused him to burn the dinner as well as his eyebrows off.",
+        borderColor: "#f1be32"
     },
     adventure: {
-        story: "With a map in hand and courage in their hearts, they set sail toward the unknown seas.",
-        borderColor: "green"
+        story: "Lost in the heart of the Amazon rain forest, Sarah and Jake stumbled upon an ancient temple. They braved deadly traps and encountered strange wildlife, all while deciphering cryptic clues left behind by a mysterious civilization.",
+        borderColor: "#acd157"
     }
 };
 
 function displayStory(genre) {
-    resultParagraph.textContent = storyObj[genre].story;
-    storyContainer.style.borderColor = storyObj[genre].borderColor;
+    if (storyObj[genre]) {
+        resultParagraph.textContent = storyObj[genre].story;
+        storyContainer.style.borderColor = storyObj[genre].borderColor;
+    }
 }
 
-scaryStoryBtn.addEventListener('click', () => displayStory('scary'));
-funnyStoryBtn.addEventListener('click', () => displayStory('funny'));
-adventureStoryBtn.addEventListener('click', () => displayStory('adventure'));
+scaryStoryBtn.addEventListener("click", () => displayStory("scary"));
+funnyStoryBtn.addEventListener("click", () => displayStory("funny"));
+adventureStoryBtn.addEventListener("click", () => displayStory("adventure"));
